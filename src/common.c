c268070e4e20a92337ab36611751a4ac829102bb
nickbash11 <nybash110998@gmail.com>
2020-11-24 16:53:52 +0300

    shared memory without locking mechanism

--- src/common.c
+++ src/common.c
@@ -1,46 +1,15 @@
 #include "raat.h"
 
 #define SIZE 65536
-#define SIZE_LOCK 4
 #define KEY 7893245
-#define KEY_LOCK 8712631
 
 void setTheInfo(pull *rcv, char *action)
 {
-	int shmid, shmid_lock;
+	int shmid;
 	char *string;
-	int  *p_lock, lock = 1, unlock = 0;
 	char buf[1024];
 
-	// get the id of segment
-	if ((shmid_lock = shmget(KEY_LOCK, SIZE_LOCK, 0644 | IPC_CREAT)) == -1)
-	{
-		perror("shmget");
-		syslog(LOG_ERR, "shmget_lock");
-		exit(-1);
-	}
- 
-	// assign the segment to the pointer
-	p_lock = shmat(shmid_lock, (int *)0, 0);
-	if (p_lock == (int *)(-1))
-	{
-		perror("shmat");
-		syslog(LOG_ERR, "shmat_lock");
-		exit(-1);
-	}
-
-	// wait while lock is 1
-	while(*p_lock == lock)
-	{
-		// sleep 10 milliseconds
-		usleep(10000);
-	}
-
-	// set lock to 1
-	*p_lock = lock;
-
 	// set the id for payload segment
-//	if ((shmid = shmget(KEY, SIZE, 0644 | IPC_CREAT)) == -1)
 	if ((shmid = shmget(KEY, SIZE, 0)) == -1)
 	{
 		syslog(LOG_ERR, "shmget");
@@ -77,9 +46,6 @@ void setTheInfo(pull *rcv, char *action)
 
 	// detach the segment
 	shmdt((void *) string);
-
-	*p_lock = unlock;
-	shmdt(p_lock);
 } 
 
 // catch for errors
